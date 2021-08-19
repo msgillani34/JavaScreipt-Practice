c7ac216e900d6b5eb695f51f387daebcdaa917fb
msgillani34 <88490248+msgillani34@users.noreply.github.com>
2021-08-19 16:36:28 +0500

    JS

---
+++ JS/app.js
@@ -0,0 +1,56 @@
+
+//var x =("Hello World!");
+
+//alert(x);
+
+//var a = ("JavaScript");
+
+// var b = (10);
+
+// var c = a + b;
+
+// alert(c);
+
+
+// var a = 10
+
+// var b = 20
+
+// var c = a + b;
+
+// alert(c);
+
+
+// var x = 10;
+
+// var y = x++ + ++x + x + --x + x-- - x;
+
+// alert(y);
+
+
+// var x = "enter your value";
+
+// var y = "your value is";
+
+// a = +prompt(x,y);
+
+// b = a + 10;
+
+// document.write( "your Value is " + "" + b );
+
+
+var age = +prompt("enter your age");
+
+if(age == 18) {
+
+    alert("you have right age to enter")
+}
+else if( age < 18) {
+
+    alert("youcan not enter")
+}
+
+else{
+
+    alert("you can enter")
+}
